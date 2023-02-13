7d75c1653ffd1f2103556860c2ebe8462f6a2d2a
Dhiraj <dhirajjadhav77777@gmial.com>
2023-02-13 19:30:39 +0530

    create the session 17 repo

---
+++ classwork/New folder/session 15/priority_queue.c
@@ -0,0 +1,25 @@
+
+#include "list.h"
+
+#define list_t prio_queue_t
+
+
+prio_queue_t* create_prio_queue(void)
+{
+    return (create_list());
+}
+
+int enqueue(prio_queue_t* p_queue)
+{
+
+}
+
+int max_dequeue(prio_queue_t* p_queue)
+{
+
+}
+
+int min_dequeue(prio_queue_t* p_queue)
+{
+    
+}
